ce2fe5df146cbbf9626435b28b33aa3901174399
José Llorens <josellorens8@gmail.com>
2024-05-08 12:33:15 -0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+    <nav>
+        <picture>
+            <img src="" alt="" />
+        </picture>
+        <div>
+            <a href="http://">Inicio</a>
+            <a href="http://">Nosotros</a>
+            <a href="http://">Contacto</a>
+            <a href="http://">Tienda</a>
+        </div>
+    </nav>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>
+        <h1>Productos destacados</h1>
+        {/* Codigo galeria js */}
+    </div>
+  )
+}
+
+export default Hero
