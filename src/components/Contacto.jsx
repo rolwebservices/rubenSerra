1eba3914fb81a2dac439ba37abb402c0acaf1c5a
José Llorens <josellorens8@gmail.com>
2024-05-23 12:14:15 -0300

    Primeros componentes

---
+++ src/components/Contacto.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Categorias.css'
+const Contacto = () => {
+  return (
+    <>
+    <h1>CONTACTO</h1>
+    {/*
+        Frame o imagen de ubicación
+    */}
+    <ul>
+        <li>030303456</li>
+        <li>dirección</li>
+        <li>correo</li>
+        </ul>
+    </>
+  )
+}
+
+export default Contacto
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import EnvironmentPlugin from 'vite-plugin-environment';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    EnvironmentPlugin(['REACT_APP_MARVEL_PUBLIC_KEY', 'REACT_APP_MARVEL_PRIVATE_KEY'])
+  ],
+  
 })
