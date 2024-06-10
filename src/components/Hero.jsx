b4ec360da22fb8b22619d9c1394c48fc91e16eb8
elzapatofreefire <elzapatofreefire@gmail.com>
2024-06-10 18:32:48 +0000

    hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import ImageGallery from 'react-image-gallery'
-import ban1 from '/ban.jpeg'
-import ban2 from '/ban2.jpeg'
 import ban3 from '/ban3.jpeg'
 import "react-image-gallery/styles/css/image-gallery.css";
 import './Hero.css'
@@ -12,8 +10,8 @@ const Hero = () => {
         <h1 className='destacados__titulo'>Productos destacados</h1>
         <ImageGallery 
         items={[
-            {original: ban1},
-            {original: ban2},
+            {original: ban3},
+            {original: ban3},
             {original: ban3}
         ]}
         
