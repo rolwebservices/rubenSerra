ffb9513245a16c19fc6fdd79e9998261eed1d7b4
José Llorens <josellorens8@gmail.com>
2024-06-18 11:52:28 -0300

    Sección Contacto lista

--- src/components/Mapa.jsx
+++ src/components/Mapa.jsx
@@ -2,17 +2,18 @@ import React from 'react'
 import { MapContainer, TileLayer, Marker } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 import './Mapa.css'
-
-
+import markerIconPng from "leaflet/dist/images/marker-icon.png"
+import {Icon} from 'leaflet'
 
 function MapView() {
+
     return (
-        <MapContainer center={{ lat: '-31.53650', lng: '-68.51857' }} zoom={15}>
+        <MapContainer center={{ lat: '-31.53650', lng: '-68.51857' }} zoom={20} >
             <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={[-31.53650, -68.51857]}>
+            <Marker position={[-31.53650, -68.51857]} icon={new Icon({iconUrl: markerIconPng, iconSize: [25, 41], iconAnchor: [12, 41]})}>
             </Marker>
         </MapContainer>
     )
